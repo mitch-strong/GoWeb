2e443027e8acb15e0eaebc15de2af1249e359b57
Richard J. de Borja <richard.deborja@uhnresearch.ca>
2018-01-25 15:23:33 -0500

    suggested feature for rotating logs

--- Web/logger.go
+++ Web/logger.go
@@ -6,6 +6,10 @@ import (
 	"time"
 )
 
+// Log files should typically be located in a rotating log file on the filesystem.
+// Investigate the possibility of having this in a /var/log (on Linux) directory
+// that rotates daily for a week.
+
 //Logger is a log that prints to the console when web pages are hit
 func Logger(inner http.Handler, name string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
